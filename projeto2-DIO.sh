c7aca00db16b1acc3f72e29a732aaed368acdb3f
adnaldo06 <129641543+adnaldo06@users.noreply.github.com>
2023-04-06 00:11:19 -0300

    Add files via upload

---
+++ projeto2-DIO.sh
@@ -0,0 +1,13 @@
+atualizar servidor
+instalar apache 2
+instalar unzip
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt install apache2 -y
+sudo apt install unzip -y
+cd /tmp
+sudo wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+sudo unzip main.zip
+sudo cp -R* /var/www/html/
+sudo rm linux-site-dio-main -r
